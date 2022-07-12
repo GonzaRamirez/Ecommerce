37ee371629932709d3310e14c1f71ef898ba3435
Gonzalo.Ramirez <gonza.ramirez08@outlook.com>
2022-07-12 18:58:15 -0300

    Aplicando Funciones

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-let numero = parseInt (prompt ("Ingrese un numero comprendido entre 1 y 10"));;
+let totalCarrito = 1
+let producto = 'producto';
 
-for (let i = 1; i <= 10; i++){
-     let multi = numero * i;
-    document.write(numero + " x " + i + " = "+ multi + "<br>")}
+precio = parseFloat(prompt("Ingresar precio")) ;
+cantidad = parseFloat(prompt("Ingresar cantidad"));
 
-    if (numero <=10){
-            alert ("¡Puedes ver las tablas de multiplicar!")
-    }
-    
-    else {
-            alert ("No puedes ingresar, ya que debes ingresar un numero entre 1 y 10")
-            window.close()
+compra = precio*cantidad;
+desc = compra*0.15;
+pagar = compra-desc;
+
+document.write("El descuento es de: $"+desc + "<br>");
+document.write("El total a pagar es: $"+pagar);
+
+function agregarAlCarrito(producto, stock) {
+        console.log('agregaste al carrito: ' + producto);
+        /* console.log('Total en el carrito de compras:' + totalCarrito++); */
+        const tenemosStock = stockDisponible(stock);
+
 }
+  
+function stockDisponible(stock){
+         
+        if(stock > 1){
+        return 'Tenemos stock';
+        }
+        else{
+        return 'No tenemos stock';}
+        
+}
+
+
+/* agregarAlCarrito('Remera Nike', 10)
+agregarAlCarrito('Zapatillas Nike', 20)
+agregarAlCarrito('Mochila', 8) */
+
+    
